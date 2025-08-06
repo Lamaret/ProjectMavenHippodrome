5d9151120c02e63898e40a9e1064d56a0c75c01c
”Lamaret” <san4eknefedov@gmail.com>
2025-08-06 18:47:54 +0300

    ProjectMaven2

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -10,7 +10,7 @@ class HorseTest {
 
     @Test
     public void constructor_NullNameParamPassed_ThrowsIllegalArgumentException() {
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Horse("1", 1, 1));
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 1, 1));
         assertEquals("Name cannot be null.", exception.getMessage());
     }
 
